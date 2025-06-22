928d021c9953d9ac0a83677fbe26bc793422ef0f
DESKTOP-R2UCRRH\SIMPATY SOLUTIONS <eric.kobby@hotmail.com>
2025-06-22 13:49:32 +0000

    Add project files.

---
+++ order-app.entities/Repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace order_app.entities.Repositories
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T? GetById(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ order-app.entities/Repositories/Implementations/Repository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace order_app.entities.Repositories.Implementations
+{
+    public class Repository<T>(DbContext ctx) : IRepository<T> where T : class
+    {
+        private readonly DbSet<T> _set = ctx.Set<T>();
+
+        public void Add(T entity)
+        {
+            _set.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _set.Remove(entity);
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _set.Where(predicate);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _set;
+        }
+
+        public T? GetById(int id)
+        {
+            return _set.Find(id);
+        }
+
+        public void Update(T entity)
+        {
+            _set.Update(entity);
+        }
+    }
+}
